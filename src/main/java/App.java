222ef266873f4a38508fbf78241077ae246cf5bd
Double Fire <double99time@gmail.com>
2023-12-21 15:20:07 +0300

    My_homeTask_done!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,23 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean0 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean0.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean1.getMessage());
+
+        System.out.println(bean0 == bean1);
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getName());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+
+        System.out.println(bean2 == bean3);
     }
 }
